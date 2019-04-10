7aebff14150c5e59d406cfa0a7aec2f2c9ecbb62
StephenAlvin <49267794+StephenAlvin@users.noreply.github.com>
2019-04-10 14:41:12 +0800

    Update and rename PODLUCK.py to Podluck Ultrasonic Sensor.py

--- Podluck Ultrasonic Sensor.py
+++ Podluck Ultrasonic Sensor.py
@@ -16,38 +16,29 @@ occupancy = ["initial"]
 
 #SETTING UP THE SERVO
 
-GPIO.setmode(GPIO.BCM) #Use the BCM GPIO numbering scheme.
-GPIO.setup(14, GPIO.OUT) #SERVO PIN
-#GPIO.setup(12, GPIO.OUT) #POWER PIN
-p = GPIO.PWM(14, 50)#PWM of 50Hz
+GPIO.setmode(GPIO.BCM)          #Use the BCM GPIO numbering scheme.
+GPIO.setup(14, GPIO.OUT)        #SERVO PIN
+#GPIO.setup(12, GPIO.OUT)       #POWER PIN
+p = GPIO.PWM(14, 50)            #PWM of 50Hz
 
 #CREATING FUNCTIONS FOR TURNING SERVOS & SENDING DATA TO FIREBASE
 
 def occupied():
-    #GPIO.output(12, 1)
     p.start(2.5)
-    p.ChangeDutyCycle(12.5) # turn towards 180 degree
-    sleep(1) # sleep 1 second
-    occupancy[0] = "occupied"
-    db.child("r_pi").set(occupancy)
+    p.ChangeDutyCycle(12.5)                     # turn towards 180 degree
+    sleep(1)                                    # sleep 1 second
+    db.child("r_pi").update({"0":"occupied"})
     print("occupied!")
-    #GPIO.output(12, 0)
     
 def not_occupied():
-    #GPIO.output(12, 1)
     p.start(2.5)
     p.ChangeDutyCycle(2.5) # turn towards 180 degree
     sleep(1) # sleep 1 second
-    occupancy[0] = "not_occupied"
-    db.child("r_pi").set(occupancy)
+    db.child("r_pi").update({"0":"occupied"})
     print("not occupied!")
-    #GPIO.output(12, 0)
 
 #SETTING UP THE SENSOR
 ultrasonic = DistanceSensor(echo=24, trigger=23, threshold_distance=0.4)
-
-#CODE THAT RUNS
-#p.start(2.5) #initialize servo
 
 while True:
         ultrasonic.wait_for_in_range()
