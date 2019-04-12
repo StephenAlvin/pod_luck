6a62c53fc764000d113cbce6b0b883a0514001f7
StephenAlvin <49267794+StephenAlvin@users.noreply.github.com>
2019-04-12 20:06:51 +0800

    Update Wireless Ultrasonic Sensor CODE.py

--- Wireless Ultrasonic Sensor CODE.py
+++ Wireless Ultrasonic Sensor CODE.py
@@ -1,8 +1,7 @@
 #IMPORTING THE LIBRARIES
 
-import RPi.GPIO as GPIO #imports pinout
-from gpiozero import DistanceSensor #imports convenient library
-from time import sleep
+import RPi.GPIO as GPIO
+from gpiozero import DistanceSensor
 from libdw import pyrebase
 
 #SETTING UP FIREBASE
@@ -13,36 +12,26 @@ config = {"apiKey": apikey,"databaseURL": url,}
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
-#SETTING UP THE SERVO
-'''
-GPIO.setmode(GPIO.BCM)          #Use the BCM GPIO numbering scheme.
-GPIO.setup(14, GPIO.OUT)        #SERVO PIN
-#GPIO.setup(12, GPIO.OUT)       #POWER PIN
-p = GPIO.PWM(14, 50)            #PWM of 50Hz
-'''
-
-#CREATING FUNCTIONS FOR TURNING SERVOS & SENDING DATA TO FIREBASE
+#CREATING FUNCTIONS FOR SENDING DATA TO FIREBASE
 
 def occupied():
-    #p.start(2.5)
-    #p.ChangeDutyCycle(12.5)                     # turn towards 180 degree
-    #sleep(1)                                    # sleep 1 second
     db.child("r_pi").update({"0":"occupied"})
     print("occupied!")
     
 def not_occupied():
-    #p.start(2.5)
-    #p.ChangeDutyCycle(2.5) # turn towards 180 degree
-    #sleep(1) # sleep 1 second
     db.child("r_pi").update({"0":"not_occupied"})
     print("not occupied!")
 
 #SETTING UP THE SENSOR
-ultrasonic = DistanceSensor(echo_pin=24, trigger_pin=23, threshold_distance=0.9)
 
-#actual script that runs
-while True: #loop forever
-        ultrasonic.wait_for_in_range() #waits for smth to come within threshold dist ans pauses the loop
+ultrasonic = DistanceSensor(echo=24, trigger=23, threshold_distance=0.4)
+
+
+#CODE THAT RUNS
+
+while True:
+        ultrasonic.wait_for_in_range()
         occupied()
         ultrasonic.wait_for_out_of_range()
         not_occupied()
+
